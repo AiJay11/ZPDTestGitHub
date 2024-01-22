1ddc5126d59779cccb0643e79b44a8c45fa6264f
cAAlis <davis.zideluns@gmail.com>
2024-01-22 20:01:41 +0200

    Revert "comment in menu script line 21"
    This reverts commit 3691cde6a903903b300a0133fab0c35cb06d02df.

--- menu.gd
+++ menu.gd
@@ -18,7 +18,7 @@ func _on_start_button_pressed():
 func _on_options_button_pressed():
 	var options = load("res://Mnus/Options.tscn").instance()
 	get_tree().current_scene.add_child(options)
-#Adians ir lohs
+
 
 func _on_quit_button_pressed():
 	get_tree().quit()
