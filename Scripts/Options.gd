d97d6e9d44ed67b228ae76d079591660fb3e9edb
AiJay11 <99174237+AiJay11@users.noreply.github.com>
2024-04-24 21:34:18 +0300

    Options simplification
    Simplified Options Screen. Added sound, fullscreen option, and V-Sync.

--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -1,41 +1,24 @@
 extends Control
 
 
-func _on_volume_changed():
-	pass # Replace with function body.
+@onready var MASTERVOLUME_BUS_ID = AudioServer.get_bus_index("MasterVolume")
+@onready var full_screen_check_box =$FullScreen_CheckBox
 
 
-func _on_music_changed():
-	pass # Replace with function body.
-
-
-func _on_full_screen_pressed():
-	pass # Replace with function body.
-
-
-func _on_window_pressed():
-	pass # Replace with function body.
+func _on_full_screen_check_box_toggled(toggled_on):
+	if toggled_on:
+		get_window().set_mode(Window.MODE_FULLSCREEN)
+	else: 
+		get_window().set_mode(Window.MODE_WINDOWED)
 
 
 func _on_v_sync_check_box_pressed():
-	pass # Replace with function body.
-
-
-func _on_latvian_pressed():
-	pass # Replace with function body.
-
-
-func _on_english_pressed():
-	pass # Replace with function body.
-
+	pass
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Menu.tscn")	
 
 
-func _on_apply_button_pressed():
-	pass # Replace with function body.
-
-
 func _on_reset_button_pressed():
-	pass # Replace with function body.
+	pass
+
